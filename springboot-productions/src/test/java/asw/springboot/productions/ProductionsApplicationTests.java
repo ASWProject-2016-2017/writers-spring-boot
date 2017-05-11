e98bf4e1a56289dc21a284ec9861812088c2c910
FedericoYusteenappar <federico.yusteenappar@gmail.com>
2017-05-11 19:06:23 +0200

    First commit

---
+++ springboot-productions/src/test/java/asw/springboot/productions/ProductionsApplicationTests.java
@@ -0,0 +1,16 @@
+package asw.springboot.productions;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductionsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
