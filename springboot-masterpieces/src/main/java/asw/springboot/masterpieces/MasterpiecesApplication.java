e74034908026f531248611cabdefeb667f77edae
Susysk <sus.marinai@hotmail.it>
2017-05-15 10:19:52 +0200

    Update MasterpiecesApplication.java

--- springboot-masterpieces/src/main/java/asw/springboot/masterpieces/MasterpiecesApplication.java
+++ springboot-masterpieces/src/main/java/asw/springboot/masterpieces/MasterpiecesApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MasterpiecesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MasterpiecesApplication.class, args);
 	}
