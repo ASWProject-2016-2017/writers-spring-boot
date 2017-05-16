60dacf29f42a6d630745575f5219d4e2c70ec850
FedericoYusteenappar <federico.yusteenappar@gmail.com>
2017-05-16 15:45:50 +0200

    deleted start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-cd springboot-writers
-echo "starting writers project"
-nohup gradle bootRun &
-cd ../springboot-masterpieces
-echo "starting masterpieces project"
-nohup gradle bootRun &
-cd ../springboot-productions
-echo "starting production project"
-nohup gradle bootRun &
