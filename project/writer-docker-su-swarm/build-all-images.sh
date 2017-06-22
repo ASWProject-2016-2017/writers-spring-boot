ca6be6ce9489594a0ab7ac96c2b614ef2e046356
FedericoYusteenappar <federico.yusteenappar@gmail.com>
2017-06-22 22:19:01 +0200

    modified docker file

--- project/writer-docker-su-swarm/build-all-images.sh
+++ project/writer-docker-su-swarm/build-all-images.sh
@@ -5,8 +5,8 @@ source "docker.env"
 # DOCKER_REGISTRY=localhost:5000
 DOCKER_REGISTRY=swarm.inf.uniroma3.it:5000
 
-docker build --rm -t ${DOCKER_REGISTRY}/eureka-server-9014 ./eureka-server
-docker build --rm -t ${DOCKER_REGISTRY}/masterpiece-9014 ./masterpiece-service
-docker build --rm -t ${DOCKER_REGISTRY}/production-9014 ./production-service
-docker build --rm -t ${DOCKER_REGISTRY}/writer-9014 ./writer-service
-docker build --rm -t ${DOCKER_REGISTRY}/writer-zuul-9014 ./zuul
+docker build --rm -t ${DOCKER_REGISTRY}/eureka-server-img-9014 ./eureka-server
+docker build --rm -t ${DOCKER_REGISTRY}/masterpiece-img-9014 ./masterpiece-service
+docker build --rm -t ${DOCKER_REGISTRY}/production-img-9014 ./production-service
+docker build --rm -t ${DOCKER_REGISTRY}/writer-img-9014 ./writer-service
+docker build --rm -t ${DOCKER_REGISTRY}/writer-zuul-img-9014 ./zuul
--- project/writer-docker-su-swarm/push-all-images.sh
+++ project/writer-docker-su-swarm/push-all-images.sh
@@ -5,8 +5,8 @@ source "docker.env"
 # DOCKER_REGISTRY=localhost:5000
 DOCKER_REGISTRY=swarm.inf.uniroma3.it:5000
 
-docker push ${DOCKER_REGISTRY}/eureka-server-9014
-docker push ${DOCKER_REGISTRY}/masterpiece-9014
-docker push ${DOCKER_REGISTRY}/production-9014
-docker push ${DOCKER_REGISTRY}/writer-9014
-docker push ${DOCKER_REGISTRY}/writer-zuul-9014
+docker push ${DOCKER_REGISTRY}/eureka-server-img-9014
+docker push ${DOCKER_REGISTRY}/masterpiece-img-9014
+docker push ${DOCKER_REGISTRY}/production-img-9014
+docker push ${DOCKER_REGISTRY}/writer-img-9014
+docker push ${DOCKER_REGISTRY}/writer-zuul-img-9014
